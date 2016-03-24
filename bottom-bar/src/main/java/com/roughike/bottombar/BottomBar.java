7b797c9ae031f2978d4f679dfa62fb223152412d
Alessandro Crugnola <crugnola@adobe.com>
2016-03-24 11:09:22 -0400

    removed import

--- bottom-bar/src/main/java/com/roughike/bottombar/BottomBar.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/BottomBar.java
@@ -24,7 +24,6 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
