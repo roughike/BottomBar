3ad721ac6d0be4699d87b6f8f771df7be670b034
Tushar Acharya <ntushar.acharya@gmail.com>
2017-03-25 02:36:35 +0530

    Fix Bottom Bar issues with Android Data Binding

--- bottom-bar/src/main/java/com/roughike/bottombar/BottomBarTab.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/BottomBarTab.java
@@ -142,7 +142,7 @@ public class BottomBarTab extends LinearLayout {
             titleView.setTextAppearance(getContext(), titleTextAppearanceResId);
         }
 
-        titleView.setTag(titleTextAppearanceResId);
+        titleView.setTag(R.id.bb_bottom_bar_appearance_id, titleTextAppearanceResId);
     }
 
     private void updateCustomTypeface() {
@@ -256,10 +256,10 @@ public class BottomBarTab extends LinearLayout {
     }
 
     int getCurrentDisplayedIconColor() {
-        Object tag = iconView.getTag();
+        Object tag = iconView.getTag(R.id.bb_bottom_bar_color_id);
 
         if (tag instanceof Integer) {
-            return (int) iconView.getTag();
+            return (int) tag;
         }
 
         return 0;
@@ -274,10 +274,10 @@ public class BottomBarTab extends LinearLayout {
     }
 
     int getCurrentDisplayedTextAppearance() {
-        Object tag = titleView.getTag();
+        Object tag = titleView.getTag(R.id.bb_bottom_bar_appearance_id);
 
         if (titleView != null && tag instanceof Integer) {
-            return (int) titleView.getTag();
+            return (int) tag;
         }
 
         return 0;
@@ -396,7 +396,7 @@ public class BottomBarTab extends LinearLayout {
         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
-               setColors((Integer) valueAnimator.getAnimatedValue());
+                setColors((Integer) valueAnimator.getAnimatedValue());
             }
         });
 
@@ -407,7 +407,7 @@ public class BottomBarTab extends LinearLayout {
     private void setColors(int color) {
         if (iconView != null) {
             iconView.setColorFilter(color);
-            iconView.setTag(color);
+            iconView.setTag(R.id.bb_bottom_bar_color_id, color);
         }
 
         if (titleView != null) {
