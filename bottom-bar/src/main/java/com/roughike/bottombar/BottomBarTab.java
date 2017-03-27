bff165e3a00f239642b152bbf7eceb346447f9f0
Marc <marcrubio89@gmail.com>
2017-03-27 23:06:59 +0100

    Added getTypedValue and getDrawableRes methods in MiscUtils and changed to selectableItemBackgroundBorderless.

--- bottom-bar/src/main/java/com/roughike/bottombar/BottomBarTab.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/BottomBarTab.java
@@ -15,7 +15,6 @@ import android.support.annotation.VisibleForTesting;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.support.v7.widget.AppCompatImageView;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -92,11 +91,7 @@ public class BottomBarTab extends LinearLayout {
         setOrientation(VERTICAL);
         setGravity(Gravity.CENTER_HORIZONTAL);
         setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-
-        // set a selectable item background
-        final TypedValue typedValue = new TypedValue();
-        getContext().getTheme().resolveAttribute(R.attr.selectableItemBackground, typedValue, true);
-        setBackgroundResource(typedValue.resourceId);
+        setBackgroundResource(MiscUtils.getDrawableRes(getContext(), R.attr.selectableItemBackgroundBorderless));
 
         iconView = (AppCompatImageView) findViewById(R.id.bb_bottom_bar_icon);
         iconView.setImageResource(iconResId);
